da6e71f1eeffd80b96c9deb2bcb21d003ee3e325
Sebastien Laroche <mr.sebas@hotmail.com>
2023-08-29 20:38:59 -0400

    learning test 4

---
+++ python/learning test/exécuter un script python libreoffice/libreoffice linux.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+cd "/usr/lib/libreoffice/program"
+python3 "/home/slaroche/Documents/GitHub/TheRockReference/python/learning test/print/print 2 (end - Fibonacci).py"
--- python/learning test/print/print 2 (end - Fibonacci).py
+++ python/learning test/print/print 2 (end - Fibonacci).py
@@ -1,10 +1,15 @@
 # l'exemple qui suit est la suite de Fibonacci
 
+# c'est une suite de nombre dont chaque terme est égal à la somme
+# des 2 termes précédents.
+
 # le end en argument dans la fonction print permet
 # de remplacer le changement de ligne par defaut 
 # a la fin du print par autre chose (ou un caractere)
 
-a, b, c = 1, 1, 1
-while c < 11:
+
+
+a, b, c = 1, 1, 1       # a et b servent au calculs des termes successifs
+while c < 11:           # c est un simple compteur
     print(b, end = " ")
     a, b, c = b, a + b, c + 1

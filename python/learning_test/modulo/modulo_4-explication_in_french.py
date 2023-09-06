dc0a9019f116c3228561585902c0f3b0748b1fbb
Sebastien Laroche <mr.sebas@hotmail.com>
2023-09-05 20:34:37 -0400

    learning test 7

---
+++ python/learning_test/modulo/modulo_4-explication_in_french.py
@@ -0,0 +1,10 @@
+
+# Le modulo correspond au reste d'une division euclidienne. 
+# C'est comme quand tu posais les divisions en primaire et 
+# que tu avais un reste à la fin.
+
+# Exemples:
+# 5%2 (cinq modulo deux) = 1 car 5 = 2x2 + 1
+# 12%25 = 12 car 12 = 0x25 + 12
+# 9%3 = 0 car 9 = 3x3 + 0
+# 129%64 = 1 car 129 = 64x2 + 1
---
+++ python/learning_test/pass_arguments_to_a_python_script/passing_args_process.py
@@ -0,0 +1,3 @@
+
+import subprocess
+subprocess.run(["python3", "/home/slaroche/Documents/GitHub/TheRockReference/python/learning_test/pass_arguments_to_a_python_script/timer.py", "5"])
